8c4b00a9dbdad080bed92588a800046abbfd8fa0
Nickolay <kolya.usmanskiy@gmail.com>
2026-03-23 19:33:53 +0300

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import github from '@astrojs/github-pages';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://Nickolazy.github.io',
+  base: '/our-story-27-03-2026',
   integrations: [react()],
+  adapter: github(),
+  output: 'static',
   vite: {
     plugins: [tailwindcss()],
     server: {
